e31e39fca8a8d5ad6a93833f2b945eb92032f8df
Eli Manzo <96manzo.eli@gmail.com>
2024-11-23 00:57:21 -0800

    Initial commit

---
+++ eslint.config.cjs
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    files: ['*.ts', '*.tsx'],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    },
+    rules: {
+      // Add your custom rules here
+    },
+  },
+];
